c23e046bd258dafcc4b8ab3c5d4d39a05cf35ce4
LOU Chaoli <ex012@qq.com>
2023-05-30 10:50:52 +0800

    Split train.py logics into functions

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 import torch.nn.functional as func
 import torch_geometric.transforms as transforms
@@ -12,19 +14,37 @@ from logger import Logger
 from evaluator import Evaluator
 from early_stopping import EarlyStopping
 
-# Experiment setup
-args = options.prepare_args()
 
-# Prepare data and model
-data, model = utils.prepare_data_and_model(args)
+def main():
+    # Experiment setup
+    args = options.prepare_args()
 
-print(model)
-weight = torch.tensor([1, args.loss_weight], dtype=torch.float32, device=args.device)
-evaluator = Evaluator(args.metrics, num_classes=args.num_classes)
-logger = Logger(settings=args)
+    # Prepare data and model
+    data, model = utils.prepare_data_and_model(args)
 
-print(f"Train started with setting {args}")
-for run in range(args.runs):
+    weight = torch.tensor([1, args.loss_weight], dtype=torch.float32, device=args.device)
+    evaluator = Evaluator(args.metrics, num_classes=args.num_classes)
+    logger = Logger(settings=args)
+
+    print(model)
+    print(f"Train started with setting {args}")
+    for run in range(args.runs):
+        _train_run(run, model, data, args, evaluator, loss_weight=weight)
+
+    print("Train ended.")
+    for result in evaluator.get_final_results():
+        print(result, end="")
+    print("")
+    if args.save_log:
+        logger.save_to_file(evaluator, path=args.log_path)
+
+
+def _train_run(run: int,
+               model: torch.nn.Module,
+               data: Data,
+               args: argparse.Namespace,
+               evaluator: Evaluator,
+               loss_weight: torch.Tensor):
     print(f"Run {run} " + "-" * 20)
     # gc.collect()
 
@@ -33,29 +53,14 @@ for run in range(args.runs):
     early_stopping = EarlyStopping(patience=args.early_stopping_patience, verbose=True)
 
     for epoch in range(args.epochs):
-        # Train
-        model.train()
-        optimizer.zero_grad()
+        train_loss = _train_epoch(model, data, optimizer, args, loss_weight=loss_weight)
+        val_loss = _validate_epoch(model, data, loss_weight=loss_weight)
+        print(f"Epoch {epoch} finished. "
+              f"train_loss: {train_loss:>7f} "
+              f"val_loss: {val_loss:>7f}")
 
-        if args.model == "dropgcn":
-            output = model(data.x, data.adj_t, drop_rate=args.drop_rate)
-        else:
-            output = model(data.x, data.adj_t)
-
-        loss = func.nll_loss(output[data.train_mask], data.y[data.train_mask],
-                             weight=weight)
-        loss.backward()
-        optimizer.step()
-        # print(f"Epoch {epoch} finished. loss: {loss.item():>7f}")
-
-        # Validation
-        model.eval()
-        predicts = model(data.x, data.adj_t)
-        val_loss = func.nll_loss(predicts[data.valid_mask], data.y[data.valid_mask],
-                                 weight=weight)
-        print(f"Epoch {epoch} finished. train_loss: {loss.item():>7f} val_loss: {val_loss.item():>7f}")
         if (args.use_early_stopping and
-                early_stopping.check_stop(val_loss.item())):
+                early_stopping.check_stop(val_loss)):
             print("Early Stopping")
             break
 
@@ -69,9 +74,37 @@ for run in range(args.runs):
         print(result, end='')
     print("")
 
-print("Train ended.")
-for result in evaluator.get_final_results():
-    print(result, end='')
-print("")
-if args.save_log:
-    logger.save_to_file(evaluator, path=args.log_path)
+
+def _train_epoch(model: torch.nn.Module,
+                 data: Data,
+                 optimizer: torch.optim.Optimizer,
+                 args: argparse.Namespace,
+                 loss_weight: torch.Tensor) -> float:
+    model.train()
+    optimizer.zero_grad()
+
+    if args.model == "dropgcn":
+        output = model(data.x, data.adj_t, drop_rate=args.drop_rate)
+    else:
+        output = model(data.x, data.adj_t)
+
+    loss = func.nll_loss(output[data.train_mask], data.y[data.train_mask],
+                         weight=loss_weight)
+    loss.backward()
+    optimizer.step()
+    # print(f"Epoch {epoch} finished. loss: {loss.item():>7f}")
+    return loss.item()
+
+
+def _validate_epoch(model: torch.nn.Module,
+                    data: Data,
+                    loss_weight: torch.Tensor) -> float:
+    model.eval()
+    predicts = model(data.x, data.adj_t)
+    val_loss = func.nll_loss(predicts[data.valid_mask], data.y[data.valid_mask],
+                             weight=loss_weight)
+    return val_loss.item()
+
+
+if __name__ == "__main__":
+    main()
