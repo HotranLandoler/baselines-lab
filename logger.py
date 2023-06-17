4cd8a959e0b551bbd70ba90566c8d5f5e0432ed0
LOU Chaoli <ex012@qq.com>
2023-06-17 14:00:43 +0800

    Change log format

--- logger.py
+++ logger.py
@@ -10,16 +10,20 @@ class Logger:
 
     def __init__(self, settings: Namespace):
         super().__init__()
-        self.model_name = settings.model.upper()
-        self.__settings = str(settings)
+        self._model_name = settings.model.upper()
+        self._dataset = settings.dataset
+        self._settings = str(settings)
 
     def save_to_file(self, evaluator: Evaluator, path: str):
         """Save current log to file"""
         with open(path, 'a', encoding="utf-8") as f:
-            f.write(f"\n\n[{datetime.now():%Y-%m-%d %H:%M:%S}]\n")
-            f.write(f"Model: {self.model_name}\n")
-            f.write(f"Settings: {self.__settings}\n")
+            f.write(f"\n[{datetime.now():%Y-%m-%d %H:%M:%S}]\n")
+            f.write(f"Model: {self._model_name}\n")
+            f.write(f"Dataset: {self._dataset}\n")
+            f.write("Note: \n")
+            f.write(f"Settings: {self._settings}\n")
             for metric, results in evaluator.metric_results.items():
                 f.write(f"{metric}: {results}\n")
             for result in evaluator.get_final_results():
                 f.write(result)
+            f.write("\n")
