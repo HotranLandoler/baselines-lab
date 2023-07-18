942aa850d86b2b3c8c0e14fd57c0ce99721b3d68
LOU Chaoli <ex012@qq.com>
2023-07-18 16:08:40 +0800

    Try use x-concat in TGAT

--- models/tgat/model.py
+++ models/tgat/model.py
@@ -10,14 +10,19 @@ from models.tgat.layers import TimeEncode
 
 class TGAT(torch.nn.Module):
     """https://github.com/hxttkl/DGraph_Experiments"""
+    time_enc: torch.nn.Module
+    lin: torch.nn.Module
+    conv: torch.nn.Module
+    out: torch.nn.Module
+
     def __init__(self, in_channels: int, out_channels: int, edge_dim=32):
         super().__init__()
         self.time_enc = TimeEncode(32)
         self.lin = torch.nn.Linear(in_channels, 32)
         self.conv = TransformerConv(32, 32 // 2, heads=2,
                                     dropout=0.1, edge_dim=edge_dim)
-        self.conv1 = TransformerConv(32, 32 // 2, heads=2,
-                                     dropout=0.1, edge_dim=edge_dim)
+        # self.conv1 = TransformerConv(32, 32 // 2, heads=2,
+        #                              dropout=0.1, edge_dim=edge_dim)
         self.out = torch.nn.Linear(32, out_channels)
 
     def forward(self, x: Tensor, edge_index: Tensor | SparseTensor, data: Data):
@@ -28,6 +33,8 @@ class TGAT(torch.nn.Module):
         h1 = F.relu(h1)
         h1 = self.conv(h1, data.edge_index, rel_t_enc)
 
+        # H2GCN D1
+        # h1 = torch.concat((data.x, h1), dim=1)
         out = self.out(h1)
         return F.log_softmax(out, dim=1)
 
@@ -35,5 +42,5 @@ class TGAT(torch.nn.Module):
         # self.time_enc.reset_parameters()
         self.lin.reset_parameters()
         self.conv.reset_parameters()
-        self.conv1.reset_parameters()
+        # self.conv1.reset_parameters()
         self.out.reset_parameters()
